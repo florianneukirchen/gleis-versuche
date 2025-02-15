1d4fed7146585ac8d2f0c76838ff66180991339b
Florian Neukirchen <mail@riannek.de>
2025-02-15 16:08:39 +0100

    First commit

---
+++ interessant.py
@@ -0,0 +1,40 @@
+import os 
+
+basedir = "/media/riannek/minimax/gleis"
+run24 = os.path.join(basedir, "2024-08-13/01/run24/01")
+run14 = os.path.join(basedir, "2024-08-14/01/run14/01")
+
+interessant = {
+    "Einfach": "4473900_5335875.copc.laz",
+    "Weiche": "4473725_5336900.copc.laz",
+    "Ende": "4473775_5336700.copc.laz",
+    "Bahnübergang": "4473950_5335400.copc.laz",
+    "Bahnübergang 2": "4474325_5332875.copc.laz",
+    "Zug run 14 A" : "4480675_5356875.copc.laz",
+    "Zug run 14 B": "4480600_5356850.copc.laz",
+    "Zug run 24 A": "4478575_5349350.copc.laz",
+    "Zug run 24 B": "4478575_5349425.copc.laz",
+    "Gebäude": "4473750_5336925.copc.laz",
+    "Gleis hohe Intensität": "4482100_5357075.copc.laz",
+    "Feldweg": "4479700_5355900.copc.laz",
+    "Feld": "4479350_5354800.copc.laz",
+    "Weiche A": "4479025_5352900.copc.laz",
+    "Weiche B": "4479025_5352925.copc.laz",
+    "Zaun": "4478775_5351900.copc.laz",
+    "Straße": "4478575_5349925.copc.laz",
+    "Kante in Graben": "4478600_5348650.copc.laz",
+    "Unterirdischer Bhf":"4476275_5343125.copc.laz",
+    "Fangschiene Tunnel": "4474500_5340125.copc.laz",
+    "Fangschiene Tunnel 2": "4473750_5338925.copc.laz",
+    "Gleis weit abseits": "4473625_5337975.copc.laz",
+    "Komische Linie": "4473725_5337650.copc.laz",
+    "Kabeltöpfe": "4473725_5337575.copc.laz",
+    "Güterzug": "4473700_5337500.copc.laz",
+    "Güterzug Ende": "4473700_5337225.copc.laz",
+    "Betondeckel": "4473700_5337175.copc.laz",
+    "Bahnsteig": "4473775_5336600.copc.laz",
+    "Bahnsteig Ende": "4473750_5336775.copc.laz",
+    "Ding neben Gleis": "4473825_5336225.copc.laz",
+    "Wände": "4474075_5333750.copc.laz",
+    "Viele Gleise": "4474750_5332150.copc.laz",
+}
